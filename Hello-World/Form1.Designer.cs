126a7db9e606a82cb76077c5ac288cce1b39611b
Kebin Wen <kew98@yahoo.com>
2018-10-03 00:15:06 -0400

    Change !!! to !

--- Hello-World/Form1.Designer.cs
+++ Hello-World/Form1.Designer.cs
@@ -38,9 +38,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(170, 112);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.Size = new System.Drawing.Size(98, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hello World !!!";
+            this.label1.Text = "Hello World !";
             // 
             // btnClose
             // 
